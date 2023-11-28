51870bf6214d00d0b6da210d3db02f085b1cc21a
Guzelis <aloyzasposka@gmail.com>
2023-11-28 18:17:18 +0200

    JS

---
+++ 8/Script.js
@@ -0,0 +1,85 @@
+//1
+let firstName = "Al",
+  lastName = "Pacino";
+
+if (firstName.length > lastName.length) {
+  console.log(lastName);
+} else {
+  firstName.length < lastName.length;
+  console.log(firstName);
+}
+
+//2
+
+let myName = "Mantvydas", myLastName = "Guzas", birthDate = "1987", thisYear = "2023";
+
+// console.log("Aš" + " " + "esu" + " " +  myName + " " + myLastName + "." + " " + "Man" + " " + "yra" + " " + (thisYear - birthDate) + " " + "metai(ų)" + "." );
+
+console.log(`Aš esu ${myName} ${myLastName}. Man yra ${thisYear - birthDate} metai(ų)`);
+
+//3
+
+let userName = "Joe", userId = "Rogan",
+
+userInicials = userName[0] + userName[1] + userId[0] + userId[1];
+
+console.log(userInicials);
+
+//4
+
+let title = "Steven", registration = "Seagal", 
+
+titleRegistration = title[3] + title[4] + title[5] + registration[3] + registration[4] + registration[5];
+
+console.log(titleRegistration);
+
+//5
+
+let date = "2023-06-23";
+
+if (date[5] + date[6] == 1) {
+  console.log("January");
+}
+if (date[5] + date[6] == 2) {
+  console.log("February");
+}
+if (date[5] + date[6] == 3) {
+  console.log("March");
+}
+if (date[5] + date[6] == 4) {
+  console.log("April");
+}
+if (date[5] + date[6] == 5) {
+  console.log("May");
+}
+if (date[5] + date[6] == 6) {
+  console.log("June");
+}
+if (date[5] + date[6] == 7) {
+  console.log("July");
+}
+if (date[5] + date[6] == 8) {
+  console.log("August");
+}
+if (date[5] + date[6] == 9) {
+  console.log("September");
+}
+if (date[5] + date[6] == 10) {
+  console.log("October");
+}
+if (date[5] + date[6] == 11) {
+  console.log("November");
+}
+if (date[5] + date[6] == 12) {
+  console.log("December");
+}
+ 
+
+
+
+
+
+
+
+
+

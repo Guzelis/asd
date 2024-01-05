3ad2801b50721153b21678e2706768f7a8c471f9
Guzelis <aloyzasposka@gmail.com>
2024-01-05 18:29:15 +0200

    cocktailApp

---
+++ cocktailApp/coctail.js
@@ -0,0 +1,12 @@
+let add = (a, b) => a + b;
+console.log(add(5, 5));
+
+let add = function (a, b) {
+  return a + b;
+};
+console.log(add(5, 5));
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(5, 5));
